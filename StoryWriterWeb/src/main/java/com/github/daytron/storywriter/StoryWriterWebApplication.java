30b54d9bb4a3f6171c6f8ec8fde847207961d686
Daytron <ryangilera@gmail.com>
2018-03-03 10:20:27 +0800

    Add Spring boot

---
+++ StoryWriterWeb/src/main/java/com/github/daytron/storywriter/StoryWriterWebApplication.java
@@ -0,0 +1,12 @@
+package com.github.daytron.storywriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryWriterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryWriterWebApplication.class, args);
+	}
+}
---
+++ StoryWriterWeb/src/test/java/com/github/daytron/storywriter/StoryWriterWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.daytron.storywriter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoryWriterWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
